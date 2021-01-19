347032074c26856bc2397369d4439ffe000e3559
LaszloLajosT <track.runner800@outlook.com>
2021-01-19 21:33:13 +0000

    Adding Safe Arguments

--- app/src/main/java/com/example/android/navigation/GameWonFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameWonFragment.kt
@@ -16,6 +16,7 @@
 
 package com.example.android.navigation
 
+
 import android.content.ActivityNotFoundException
 import android.content.Intent
 import androidx.databinding.DataBindingUtil
@@ -45,10 +46,15 @@ class GameWonFragment : Fragment() {
         binding.nextMatchButton.setOnClickListener { view: View ->
             // TODO (10) Replace action ID with actionGameWonFragmentToGameFragment
             // From GameWonFragmentDirections
-            view.findNavController().navigate(R.id.action_gameWonFragment_to_gameFragment)
+            view.findNavController().navigate(GameWonFragmentDirections.actionGameWonFragmentToGameFragment())
         }
-        // TODO (08) Add and show toast to get the GameWonFragmentArgs from the arguments Bundle
-        // "NumCorrect: ${args.numCorrect}, NumQuestions: ${args.numQuestions}"
+        var args = arguments?.let { GameWonFragmentArgs.fromBundle(it) }
+
+        args?.let {
+            Toast.makeText(context,
+                    "NumCorrect: ${args.numCorrect},NumQuestions: ${args.numQuestions}",
+                    Toast.LENGTH_LONG).show()
+        }
         return binding.root
     }
 }
